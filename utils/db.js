daff81c0a1b0c03563bf3ccd3baf4569ceac2765
kimaita <johnnieqaym@gmail.com>
2024-10-15 00:27:42 +0300

    added mongodb manager class

---
+++ utils/db.js
@@ -0,0 +1,42 @@
+import { MongoClient } from 'mongodb';
+
+class DBClient {
+  constructor() {
+    const host = process.env.DB_HOST || 'localhost';
+    const port = process.env.DB_PORT || '27017';
+    const uri = `mongodb://${host}:${port}`;
+
+    const dbName = process.env.DB_DATABASE || 'files_manager';
+
+    this.isConnected = false;
+
+    (async () => {
+      this.client = new MongoClient(uri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      });
+
+      await this.client.connect();
+      this.db = this.client.db(dbName);
+      this.users = this.db.collection('users');
+      this.files = this.db.collection('files');
+      await this.db.command({ ping: 1 });
+      this.isConnected = true;
+    })();
+  }
+
+  isAlive() {
+    return this.isConnected;
+  }
+
+  async nbUsers() {
+    return this.users.countDocuments();
+  }
+
+  async nbFiles() {
+    return this.files.countDocuments();
+  }
+}
+
+const dbClient = new DBClient();
+export default dbClient;
